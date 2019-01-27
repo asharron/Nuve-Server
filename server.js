8a573b1a41b15bfd97c85c001eec635f8ada7309
asharron <alexsharron@gmail.com>
2019-01-27 14:51:33 -0500

    Create basic video serving along with sample video

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
+//Test data structure 
+//TODO: Commit this to DB so it doesn't have to build on start every time
+var filemap = {};
+
+function buildMap() {
+	//Read the files in the video directory
+	fs.readdir(path.join(__dirname, "/videos"), (err, files) => {
+
+		//Create a url for each one
+		files.forEach((name) => {
+			filemap[name.split(".")[0]] = "http://localhost:8080/videos/" + name;
+		});
+		console.log(filemap);
+		console.log(filemap["sample"]);
+	});
+}
+
+buildMap();
+
 
 //Serves up the built React files
 app.use(express.static(path.join(__dirname, '../Nuve-UI/build')));
+//Serves up video files based on url
+app.use("/videos", express.static(path.join(__dirname, "/videos")));
 
+//API endpoint to return URL of video
+app.get('/video', (req, res) => {
+	console.log("Received request!");
+	res.send({ url: filemap["sample"] });
+});
+
+
+//Start the server
 app.listen(8080, () => {
 	console.log("Listening on 8080");
 });
